74303cb5b19c6a6adb2a73ac7381877b1598ac55
Ketan Dudka <ketandudka566123@gmail.com>
2025-05-28 10:50:46 +0530

    push

--- src/app/addActivity/page.js
+++ src/app/addActivity/page.js
@@ -6,12 +6,11 @@ import { LuMinus } from "react-icons/lu";
 import "./addActivity.scss";
 import axios from "axios";
 import { useRouter, useSearchParams } from "next/navigation";
-import Image from "next/image";
 
 const Page = () => {
+  const [errors, setErrors] = useState({});
   const searchParams = useSearchParams();
-  
-  const id = searchParams.get("acivityId");
+  const id = searchParams.get("activityId");
   const router = useRouter();
   const [addData, setAddData] = useState({
     title: "",
@@ -81,16 +80,15 @@ const Page = () => {
   }, [keyValuePairs]);
 
   const addActivity = async () => {
-    if (!validate()) return;
     try {
       const formdata = new FormData();
 
       if (id) {
         formdata.append("data", JSON.stringify({ ...addData, aid: id }));
-      } else {
+      }else{
         formdata.append("data", JSON.stringify(addData));
       }
-
+     
       formdata.append("image", image.image);
       formdata.append("coverImage", coverImage.coverImage);
       let response;
@@ -107,8 +105,9 @@ const Page = () => {
         );
       }
 
+      console.log(response)
       if (response.status === 201) {
-        router.push("/activity");
+       router.push("/activity")
       }
     } catch (error) {
       console.log(error);
@@ -120,9 +119,6 @@ const Page = () => {
         `${process.env.NEXT_PUBLIC_API_URL}/activity/getById?activityId=${id}`
       );
 
-      console.log(response,">>>>response");
-      
-
       setAddData({
         title: response.data.data.title,
         activityTitle: response.data.data.activityTitle,
@@ -130,12 +126,12 @@ const Page = () => {
       });
 
       setimage({
-        image: null,
+        image:null,
         imagePrev: response.data.data.image,
       });
 
       setCoverImage({
-        coverImage: null,
+        coverImage:null,
         coverImagePrev: response.data.data.coverImage,
       });
       const additionalInfoArray = Object.entries(
@@ -157,273 +153,178 @@ const Page = () => {
     }
   }, [id]);
 
-  const [error, setError] = useState({
-    title: "",
-    activityTitle: "",
-    description: "",
-  });
-
-  const validate = () => {
-    let errors = {};
-
-    if (!addData.title.trim()) errors.title = "title is required";
-    else if (addData.title.length > 50) errors.title = "title is too long";
-
-    if (!addData.description.trim())
-      errors.description = "description is required";
-    else if (addData.description.length > 550)
-      errors.description = "description is too long";
-
-    if (!addData.activityTitle.trim())
-      errors.activityTitle = "activity title is required";
-    else if (addData.description.length > 550)
-      errors.activityTitle = "activity title is too long";
-
-    if (keyValuePairs.length < 3)
-      errors.additionalInfo =
-        "At least 3 activity key-value pairs are required";
-
-    keyValuePairs.forEach((pair, index) => {
-      if (!pair.key.trim()) errors[`key_${index}`] = "Key is required";
-      else if (pair.key.length > 50) errors[`key_${index}`] = "Key is too long";
-
-      if (!pair.value.trim()) errors[`value_${index}`] = "Value is required";
-      else if (pair.value.length > 200)
-        errors[`value_${index}`] = "Value is too long";
-    });
-
-    if (!image.image) {
-      errors.image = "Feature image is required";
-    } else {
-      const allowedFormats = ["image/jpeg", "image/png", "image/jpg"];
-      if (!allowedFormats.includes(image.image.type)) {
-        errors.image = "Feature image must be in JPG, PNG, or JPEG format";
-      }
-      if (image.image.size > 5 * 1024 * 1024) {
-        errors.image = "Feature image must be less than 5MB";
-      }
-    }
-
-    if (!coverImage.coverImage) {
-      errors.coverImage = "Cover image is required";
-    } else {
-      const allowedFormats = ["image/jpeg", "image/png", "image/jpg"];
-      if (!allowedFormats.includes(coverImage.coverImage.type)) {
-        errors.coverImage = "Cover image must be in JPG, PNG, or JPEG format";
-      }
-      if (coverImage.coverImage.size > 5 * 1024 * 1024) {
-        errors.coverImage = "Cover image must be less than 5MB";
-      }
-    }
-
-    setError(errors);
-    return Object.keys(errors).length === 0;
-  };
-
-
-  console.log("hello rishabh");
-  
-
   return (
     <>
-
-        <div className="parent homepage">
-          <div className="container homepage-container ">
-            <div className="header">
-              <div className="title">
-                <div className="back"></div>
-                <h2>Add Acitivty Page Content </h2>
-              </div>
-              <div className="btns">
-                <button className="btn " onClick={addActivity}>
-                  {id ? "Update" : "Save"}
-                </button>
-                <button className="btn2 ">Reset</button>
-              </div>
+      <div className="parent homepage">
+        <div className="container homepage-container ">
+          <div className="header">
+            <div className="title">
+              <div className="back"></div>
+              <h2>Add Acitivty Page Content </h2>
             </div>
-            <form>
-              <div className="left">
-                <div className="row">
-                  <label>
-                    <div className="top">
-                      <p>Title *</p>
+            <div className="btns">
+              <button className="btn " onClick={addActivity}>
+                {id ? "Update" : "Save"}
+              </button>
+              <button className="btn2 ">Reset</button>
+            </div>
+          </div>
+          <form>
+            <div className="left">
+              <div className="row">
+                <label>
+                  <div className="top">
+                    <p>Title *</p>
+                  </div>
+                  <input
+                    placeholder="your activity title goes here..."
+                    type="text"
+                    value={addData.title}
+                    onChange={(e) =>
+                      setAddData({ ...addData, title: e.target.value })
+                    }
+                  />
+                </label>
+                <label>
+                  <div className="top">
+                    <p>Activity Title *</p>
+                    <div
+                      className={`counter ${
+                        addData?.title?.length > 49 ? "error" : ""
+                      }`}
+                    >
+                      {addData?.title?.length}/50 character
                     </div>
-                    <input
-                      placeholder="your activity title goes here..."
-                      type="text"
-                      value={addData.title}
-                      onChange={(e) =>
-                        setAddData({ ...addData, title: e.target.value })
-                      }
-                    />
-                    {error.title && (
-                      <span className="error">{error.title}</span>
-                    )}
-                  </label>
+                  </div>
+                  <input
+                    placeholder="your main activity title goes here..."
+                    type="text"
+                    value={addData.activityTitle}
+                    onChange={(e) =>
+                      setAddData({ ...addData, activityTitle: e.target.value })
+                    }
+                  />
+                </label>
+              </div>
+              <div className="row">
+                <label>
+                  <div className="top">
+                    <p>Activity Description*</p>
+                    <div className="counter">10/10</div>
+                  </div>
+                  <textarea
+                    placeholder="your activity description goes here..."
+                    type="text"
+                    value={addData.description}
+                    onChange={(e) =>
+                      setAddData({ ...addData, description: e.target.value })
+                    }
+                  />
+                </label>
+              </div>
+              {keyValuePairs.map((pair, index) => (
+                <div className="row" key={index}>
                   <label>
                     <div className="top">
-                      <p>Activity Title *</p>
-                      <div
-                        className={`counter ${
-                          addData?.title?.length > 49 ? "error" : ""
-                        }`}
-                      >
-                        {addData?.title?.length}/50 character
-                      </div>
-                    </div>
-                    <input
-                      placeholder="your main activity title goes here..."
-                      type="text"
-                      value={addData.activityTitle}
-                      onChange={(e) =>
-                        setAddData({
-                          ...addData,
-                          activityTitle: e.target.value,
-                        })
-                      }
-                    />
-                    {error.activityTitle && (
-                      <span className="error">{error.activityTitle}</span>
-                    )}
-                  </label>
-                </div>
-                <div className="row">
-                  <label>
-                    <div className="top">
-                      <p>Activity Description*</p>
+                      <p>Activity Key*</p>
                       <div className="counter">10/10</div>
                     </div>
-                    <textarea
-                      placeholder="your activity description goes here..."
+                    <input
                       type="text"
-                      value={addData.description}
+                      placeholder="Enter key"
+                      value={pair.key}
                       onChange={(e) =>
-                        setAddData({ ...addData, description: e.target.value })
+                        handleInputChange(index, "key", e.target.value)
                       }
                     />
-                    {error.description && (
-                      <span className="error">{error.description}</span>
-                    )}
                   </label>
-                </div>
-                {keyValuePairs.map((pair, index) => (
-                  <div className="row" key={index}>
-                    <label>
-                      <div className="top">
-                        <p>Activity Key*</p>
-                        <div className="counter">10/10</div>
-                      </div>
+
+                  <label>
+                    <div className="top">
+                      <p>Activity Value*</p>
+                      <div className="counter">10/10</div>
+                    </div>
+                    <div className="append_button">
                       <input
                         type="text"
-                        placeholder="Enter key"
-                        value={pair.key}
+                        placeholder="Enter value"
+                        value={pair.value}
                         onChange={(e) =>
-                          handleInputChange(index, "key", e.target.value)
+                          handleInputChange(index, "value", e.target.value)
                         }
                       />
-                      {error[`key_${index}`] && (
-                        <span className="error">{error[`key_${index}`]}</span>
-                      )}
-                    </label>
-
-                    <label>
-                      <div className="top">
-                        <p>Activity Value*</p>
-                        <div className="counter">10/10</div>
+                      <div className="plus_button" onClick={handleAddRow}>
+                        {" "}
+                        <GoPlus />{" "}
                       </div>
-                      <div className="append_button">
-                        <input
-                          type="text"
-                          placeholder="Enter value"
-                          value={pair.value}
-                          onChange={(e) =>
-                            handleInputChange(index, "value", e.target.value)
-                          }
-                        />
-                        <div className="plus_button" onClick={handleAddRow}>
+                      {index > 0 && (
+                        <div
+                          className="minus_button"
+                          onClick={() => handleRemoveRow(index)}
+                        >
                           {" "}
-                          <GoPlus />{" "}
+                          <LuMinus />{" "}
                         </div>
-                        {index > 0 && (
-                          <div
-                            className="minus_button"
-                            onClick={() => handleRemoveRow(index)}
-                          >
-                            {" "}
-                            <LuMinus />{" "}
-                          </div>
-                        )}
-                      </div>
-                      {error[`value_${index}`] && (
-                        <span className="error">{error[`value_${index}`]}</span>
                       )}
-                    </label>
+                    </div>
+                  </label>
+                </div>
+              ))}
+              <div className="row">
+                <label>
+                  <div className="top">
+                    <p>Feature Image *</p>
                   </div>
-                ))}
-                <div className="row">
-                  <label>
-                    <div className="top">
-                      <p>Feature Image *</p>
-                    </div>
-                    <input
-                      placeholder="your main hero heading goes here..."
-                      type="file"
-                      onChange={(e) => handleFileChange(e, "feature")}
-                    />
-                    {error.image && (
-                      <span className="error">{error.image}</span>
-                    )}
-                  </label>
-                  <label>
-                    <div className="top">
-                      <p>Cover Image *</p>
-                    </div>
-                    <input
-                      placeholder="your main hero heading goes here..."
-                      type="file"
-                      onChange={(e) => handleFileChange(e, "cover")}
-                    />
-                    {error.coverImage && (
-                      <span className="error">{error.coverImage}</span>
-                    )}
-                  </label>
+                  <input
+                    placeholder="your main hero heading goes here..."
+                    type="file"
+                    onChange={(e) => handleFileChange(e, "feature")}
+                  />
+                </label>
+                <label>
+                  <div className="top">
+                    <p>Cover Image *</p>
+                  </div>
+                  <input
+                    placeholder="your main hero heading goes here..."
+                    type="file"
+                    onChange={(e) => handleFileChange(e, "cover")}
+                  />
+                </label>
+              </div>
+            </div>
+            <div className="right">
+              <div className="video-select">
+                <div
+                  className={`option ${
+                    controls.view === "feature" ? "active" : ""
+                  }`}
+                  onClick={() => setControls({ ...controls, view: "feature" })}
+                >
+                  Feature Image
+                </div>
+                <div
+                  className={`option ${
+                    controls.view === "cover" ? "active" : ""
+                  }`}
+                  onClick={() => setControls({ ...controls, view: "cover" })}
+                >
+                  Cover Image
                 </div>
               </div>
-              <div className="right">
-                <div className="video-select">
-                  <div
-                    className={`option ${
-                      controls.view === "feature" ? "active" : ""
-                    }`}
-                    onClick={() =>
-                      setControls({ ...controls, view: "feature" })
-                    }
-                  >
-                    Feature Image
-                  </div>
-                  <div
-                    className={`option ${
-                      controls.view === "cover" ? "active" : ""
-                    }`}
-                    onClick={() => setControls({ ...controls, view: "cover" })}
-                  >
-                    Cover Image
-                  </div>
-                </div>
-                <div className={`video-box ${controls.view}`}>
-                  {controls.view === "feature" && image.imagePrev && (
-                    <Image src={image.imagePrev} alt="image" />
-                  )}
+              <div className={`video-box ${controls.view}`}>
+                {controls.view === "feature" && (
+                  <img src={image.imagePrev} alt="" />
+                )}
 
-                  {controls.view === "cover" && coverImage.coverImagePrev && (
-                    <Image src={coverImage.coverImagePrev} alt="image" />
-                  )}
-                </div>
+                {controls.view === "cover" && (
+                  <img src={coverImage.coverImagePrev} alt="" />
+                )}
               </div>
-            </form>
-          </div>
+            </div>
+          </form>
         </div>
-    
+      </div>
     </>
   );
 };
