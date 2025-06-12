de67200a2c984b9661dedcdf20c7b84e49531663
Ketan Dudka <ketandudka566123@gmail.com>
2025-06-12 13:03:30 +0530

    push

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -1,15 +1,174 @@
-import React from 'react'
-import "./Gallery.scss"
+"use client";
+
+import React, { useEffect, useState } from "react";
+import "./Gallery.scss";
+import { MdModeEditOutline } from "react-icons/md";
+import axios from "axios";
+import { MdCloudUpload } from "react-icons/md";
+import { MdDelete } from "react-icons/md";
 const Page = () => {
+  const [images, setImages] = useState();
+  const [popup, setPopUp] = useState(false);
+  const [imagefile, setImageFile] = useState();
+  const [editId, setEditId] = useState();
+  const [delayeMessage, setDelayedMessage] = useState(false)
+
+  const getAllImages = async () => {
+    try {
+      const response = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_URL}/MediaController/getAllImage`
+      );
+
+      console.log(response.data);
+
+      setImages(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getAllImages();
+  }, []);
+
+  // add image
+
+  const addImage = async (e) => {
+    try {
+      e.preventDefault();
+      let response;
+      const formData = new FormData();
+
+      formData.append("file", imagefile);
+
+      if (editId) {
+        formData.append("mId", editId);
+      }
+
+      if (editId) {
+        response = await axios.post(
+          `${process.env.NEXT_PUBLIC_API_URL}/MediaController/updateImage`,
+          formData
+        );
+      } else {
+        response = await axios.post(
+          `${process.env.NEXT_PUBLIC_API_URL}/MediaController/uploadImage`,
+          formData
+        );
+      }
+
+      if (response.status === 200) {
+        setDelayedMessage(true)
+        getAllImages();
+        setImageFile(null);
+        setPopUp(false);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // openedit Popup
+
+  const openEditPopUp = (id) => {
+    setEditId(id);
+    setPopUp(true);
+  };
+
+  // delete image
+
+  const deleteImage = async (id) => {
+    try {
+      const confirm = window.confirm("Are You sure want to delete this image?");
+
+      if (!confirm) return;
+
+      const response = await axios.delete(
+        `${process.env.NEXT_PUBLIC_API_URL}/MediaController/deleteImage?mId=${id}`
+      );
+
+      if (response.status === 200) {
+        getAllImages();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
+      {/* popup */}
+      {popup && (
+        <div class="popup">
+          <div class="overlay" onClick={() => setPopUp(false)}></div>
+          <form action="" className="form" onSubmit={addImage}>
+            <div class="upload_image">
+              <MdCloudUpload />
+              <p>Upload Image</p>
+              <input
+                type="file"
+                onChange={(e) => setImageFile(e.target.files[0])}
+              />
+            </div>
+            <button className="btn" type="submit">
+              Add Image
+            </button>
+          </form>
+        </div>
+      )}
+
       <div class="gallery parent">
         <div class="gallery_cont container">
+          <div class="header">
+            <div class="title">
+              <h2>Gallery</h2>
+            </div>
+           
+              {
+                delayeMessage &&
+                <p>
+                  The Gallery is currently under maintenance. Please check 1 min later.
+                </p>
+              }
+            
+            <div class="btns">
+              <button className="btn" onClick={() => setPopUp(true)}>
+                Add Gallery
+              </button>
+            </div>
+          </div>
 
+          <div class="gallery_list">
+            {images &&
+              images.map((item, index) => (
+                <>
+                  <div
+                    class="image "
+                    key={index}
+                    style={{ backgroundImage: `url(${item?.data?.image})` }}
+                  >
+                    <div class="overlay">
+                      <div
+                        class="icon"
+                        onClick={() => openEditPopUp(item?.data?.mid)}
+                      >
+                        <MdModeEditOutline />
+                      </div>
+                      <div
+                        class="icon"
+                        onClick={() => deleteImage(item?.data?.mid)}
+                      >
+                        <MdDelete />
+                      </div>
+                    </div>
+                  </div>
+                </>
+              ))}
+          </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
